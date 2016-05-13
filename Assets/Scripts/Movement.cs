e0044326f1c492b7e569321869a94bb05e22132c
sohanalva87 <sohan.alva87@gmail.com>
2016-05-13 17:38:57 -0400

    Photon Networking

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,25 +4,22 @@ using UnityEngine.UI;
 
 public class Movement : MonoBehaviour {
 	public float speed;
+	public bool onLadder;
 	public Rigidbody2D rb;
 	public float jumpForce;
 	Vector3 move;
 	bool onGround;
 	public GameObject red;
-	bool onLadder;
-	public bool canGoUp;
-	public bool canGoDown;
-	public bool onNoJumpArea;
-	public GameObject topPlatform;
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
-
 	// Update is called once per frame
 	void Update () 
 	{
+		
 		if (rb.velocity.y > 0) 
 		{
 			onGround = false;
@@ -32,19 +29,13 @@ public class Movement : MonoBehaviour {
 		{
 			onGround = true;
 		}
-		if ((onLadder)&&(canGoUp)&&((Input.GetAxis("Vertical")>0))) {
-			move = new Vector3 (0, Input.GetAxis ("Vertical"), 0);
-			transform.position += move * speed * Time.deltaTime;
-		}
-		if ((onLadder)&&(canGoDown)&&((Input.GetAxis("Vertical")<0))) {
-			move = new Vector3 (0, Input.GetAxis ("Vertical"), 0);
-			transform.position += move * speed * Time.deltaTime;
-		}
-			move = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
-			transform.position += move * speed * Time.deltaTime;
-		
+
+		move = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+
+		transform.position += move * speed * Time.deltaTime;
+
 		// Jump if on ground
-		if ((Input.GetKeyDown (KeyCode.Space)&&(onGround))&&(!onNoJumpArea))
+		if (Input.GetKeyDown (KeyCode.Space)&&(onGround))
 		{
 			rb.AddForce (Vector2.up*jumpForce,ForceMode2D.Impulse);
 		}
@@ -63,56 +54,21 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
-
 	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.tag == "NoJumpArea") {
-			onNoJumpArea = true;
-		}
-
-		if (other.tag == "Ladder") {
-			
-			onLadder = true;
-			rb.gravityScale = 0;
-			if (other.gameObject.transform.position.y < gameObject.transform.position.y)
-				canGoUp = false;
-		}
-		if (other.gameObject.tag == "BottomLadder") {
-			canGoDown = false;
-			canGoUp = true;
-		}
-		if (other.gameObject.tag == "TopLadder") {
-			canGoDown = true;
-			Physics2D.IgnoreCollision (gameObject.GetComponent<Collider2D>(),
-				topPlatform.GetComponent<Collider2D>(),true);
-			
-		}
-	}
-
-	void OnTriggerStay2D(Collider2D other)
 	{
 		
 	}
 
 	void OnTriggerExit2D(Collider2D other)
 	{
-		if (other.tag == "NoJumpArea") {
-			onNoJumpArea = false;
-		}
+		
+	}
 
-		if (other.tag == "Ladder") {
-			onLadder = false;
-			rb.gravityScale = 1;
-			canGoUp = false;
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		
+	}
 
-		}
-		if (other.gameObject.tag == "BottomLadder") {
-			canGoDown = true;
-		}
-		if (other.gameObject.tag == "TopLadder") {
-			Physics2D.IgnoreCollision (gameObject.GetComponent<Collider2D>(),
-				topPlatform.GetComponent<Collider2D>(),false);
-			canGoUp = true;
-		}
-	}		
+
+		
 }
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Connect ();
+	}
+	
+	// Update is called once per frame
+	void Connect () {
+		Debug.Log ("Connect()");
+		PhotonNetwork.ConnectUsingSettings ("1.0.0");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log ("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnConnectedToMaster(){
+
+		Debug.Log ("OnConnectedToMaster");
+	}
+
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom (null);
+
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("OnJoinedRoom");
+
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer(){
+		GameObject MyPlayerGO = (GameObject)PhotonNetwork.Instantiate ("Player", Vector3.zero, Quaternion.identity, 0);
+		((MonoBehaviour)MyPlayerGO.GetComponent ("Movement")).enabled = true;
+
+	}
+
+}
