51e4cd3e9beef7c8c8ead8c0c673baab7763494c
Artem Nikolaev <oektann@gmail.com>
2016-05-13 17:17:04 -0400

    Ladder stuff, control improvements, design improvements.

--- Assets/Scripts/ladder.cs
+++ Assets/Scripts/ladder.cs
@@ -1,36 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ladder : MonoBehaviour {
-	public GameObject playerObject;
-	public bool canClimb;
-	float speed = 1;
-
+public class Ladder : MonoBehaviour {
+	public GameObject upPlatform;
+	private Movement movement;
 	// Use this for initialization
 	void Start () {
-		
+		movement = GetComponent<Movement> ();
 	}
-
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-		if(coll.gameObject == playerObject)
-			canClimb = true;
-	}
-
-	void OnCollisionExit2d(Collision2D coll2)
-	{
-		if (coll2.gameObject == playerObject)
-			canClimb = false;
-	}
+	
 	// Update is called once per frame
 	void Update () {
-		if (canClimb) {
-			if (Input.GetKey (KeyCode.W)) {
-				playerObject.transform.Translate (new Vector3 (0, 1, 0) * Time.deltaTime * speed);
-			}
-			if (Input.GetKey (KeyCode.S)) {
-				playerObject.transform.Translate (new Vector3 (0, -1, 0) * Time.deltaTime * speed);
-			}
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			other.gameObject.GetComponent<Movement> ().topPlatform = upPlatform;
 		}
 	}
 }
