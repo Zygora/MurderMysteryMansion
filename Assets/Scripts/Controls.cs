e3aef498d0c09ba66b1f58e8c7254affe64ecb45
Artem Nikolaev <oektann@gmail.com>
2016-05-18 17:31:09 -0400

    Minor fixes for controls + comments

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -2,13 +2,25 @@
 using System.Collections;
 
 public class Controls : MonoBehaviour {
-    public float speed;
-    public Rigidbody2D  rb;
-    public GameObject red;
-    public float jumpForce;
+    // Tweakable variables
+    [Header("Tweakable variables")]
+    [Range(1, 15)]
+    public float playerSpeed;
+    [Range(20, 70)]
+    public float playerJumpForce;
+    [Range(1, 15)]
+    public float playerClimbSpeed;
+    [Space(10), ]
+    // System variables
+    private Rigidbody2D rb;
+    private Vector3  move;
+    // References to other objects (leave public)
+    [Header("References")]
     public GameObject topPlatform;
-
-    Vector3  move;
+    public GameObject redBackground;
+    [Space(10)]
+    // Ladder booleans (make private at the end)
+    [Header("Ladder booleans")]
     public bool onLadder;
     public bool onGround;
     public bool onNoJumpArea;
@@ -16,13 +28,11 @@ public class Controls : MonoBehaviour {
     public bool canGoDown;
     public bool onTop;
 
-    // Use this for initialization
     void Start () {
         // Get rigidbody of the player at start
         rb = GetComponent<Rigidbody2D>();
     }
 	
-	// Update is called once per frame
 	void Update () {
         // Create a ray down checking if there is anything underneath the player
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.57f);
@@ -36,62 +46,64 @@ public class Controls : MonoBehaviour {
         {
             onGround = false;
         }
+        // If player is on a ladder can go down -> go down
         if ((onLadder) && (canGoDown) && ((Input.GetAxis("Vertical") < 0)))
         {
             move = new Vector3(0, Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed * Time.deltaTime;
+            transform.position += move * playerClimbSpeed * Time.deltaTime;
         }
-
+        // If player is on a ladder and not at the top and can go up -> go up
         if ((onLadder) && (canGoUp) && ((Input.GetAxis("Vertical") > 0))&&(!onTop))
         {
             move = new Vector3(0, Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed * Time.deltaTime;
+            transform.position += move * playerClimbSpeed * Time.deltaTime;
         }
-
         // Create move vector
         move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         // Move the player
-        transform.position += move * speed * Time.deltaTime;
-
-
+        transform.position += move * playerSpeed * Time.deltaTime;
         // If player is on ground and space button hit -> jump
         if (Input.GetKeyDown(KeyCode.Space) && (onGround)&&(!onNoJumpArea))
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * playerJumpForce, ForceMode2D.Impulse);
             Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(),
                 topPlatform.GetComponent<Collider2D>(), false);
         }
         // Special Ability
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (red.activeSelf)
+            if (redBackground.activeSelf)
             {
-                red.SetActive(false);
+                redBackground.SetActive(false);
                 return;
             }
-            if (!red.activeSelf)
+            if (!redBackground.activeSelf)
             {
-                red.SetActive(true);
+                redBackground.SetActive(true);
                 return;
             }
         }
     }
+
     void OnTriggerStay2D(Collider2D other)
     {
+        // While player is at the bottom of a ladder they can't go down and can go up
         if (other.gameObject.tag == "BotLadder")
         {
             canGoDown = false;
             canGoUp = true;
         }
+        // Make player's mass 0 if they are on ladder
         if (other.tag == "Ladder")
         {
             rb.gravityScale = 0;
         }
+        // At the top of a ladder turn off the collision of a player and the platform above them
         if (other.tag == "TopLadder")
         {
+            canGoUp = true;
             onLadder = true;
-            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(),
-               topPlatform.GetComponent<Collider2D>(), true);
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), topPlatform.GetComponent<Collider2D>(), true);
         }
     }
 
@@ -101,7 +113,8 @@ public class Controls : MonoBehaviour {
         {
             onTop = true;
         }
-            if (other.tag == "NoJumpArea")
+        // Player cant jump on ladder, can go up and down. Gravity scale = 0 so they can climb up
+        if (other.tag == "NoJumpArea")
         {
             onNoJumpArea = true;
             canGoUp = true;
@@ -109,20 +122,14 @@ public class Controls : MonoBehaviour {
             rb.velocity = new Vector3(0, 0, 0);
             rb.gravityScale = 0;
         }
-        if (other.tag=="Ladder")
+        // When player enters a ladder make his velocity 0 and turn off gravity scale
+        if (other.tag == "Ladder")
         {
+            rb.velocity = new Vector3(0, 0, 0);
             onLadder = true;
             rb.gravityScale = 0;
-            if (other.gameObject.transform.position.y < gameObject.transform.position.y)
-            {
-                canGoUp = false;
-            }
-
-    }
-        if (other.gameObject.tag == "TopLadder")
-        {
-            canGoDown = true;
         }
+        // At the bottom of a ladder player can go up, but not down
         if (other.gameObject.tag == "BotLadder")
         {
             canGoDown = false;
@@ -154,8 +161,7 @@ public class Controls : MonoBehaviour {
         }
         if (other.gameObject.tag == "TopLadder")
         {
-                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(),
-                   topPlatform.GetComponent<Collider2D>(), false);
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), topPlatform.GetComponent<Collider2D>(), false);
             rb.gravityScale = 1;
         }
     }
---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS : MonoBehaviour
+{
+
+    string label = "";
+    float count;
+    private GUIStyle guiStyle = new GUIStyle();
+
+    IEnumerator Start()
+    {
+        GUI.depth = 2;
+        while (true)
+        {
+            if (Time.timeScale == 1)
+            {
+                yield return new WaitForSeconds(0.1f);
+                count = (1 / Time.deltaTime);
+                label = "FPS: " + (Mathf.Round(count));
+            }
+            else
+            {
+                label = "Pause";
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void OnGUI()
+    {
+        guiStyle.normal.textColor = Color.red;
+        guiStyle.fontSize = 20;
+        GUI.Label(new Rect(0, 0, 400, 100), label, guiStyle);
+    }
+}
