a3feb2e2025335442fb223f785d165cd1ba68b01
sohan <sohan>
2016-05-18 14:33:45 -0400

    Smoothing network movement. Added NetworkCharacter script.

---
+++ Assets/Scripts/NetworkCharacter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting) // Our player. We need to send our actual position to the network.
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            //this is someone else's player. We need to receive their position (as of a few milliseconds ago, and update our version of that player.
+
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+
+        }
+
+    }
+}
