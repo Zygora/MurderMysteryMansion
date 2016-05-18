59484285005b7ddb75bb398a0157d656bffe032f
Eric Phangnanouvong <Viruth_e-phang@hotmail.com>
2016-05-18 18:57:26 -0400

    New player Prefab and Animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,12 @@ public class Movement : MonoBehaviour {
     Vector3 move;
 
     public float speed = 1.0f;
+    Animator Animator;
 
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
     void Update()
     {
 
@@ -13,6 +18,34 @@ public class Movement : MonoBehaviour {
 
         transform.position += move * speed * Time.deltaTime;
 
+        if (Input.GetAxis("Horizontal") > -0.5f && Input.GetAxis("Horizontal") < 0.5f)
+        {
+            Animator.SetBool("Running", false);
+            Animator.SetBool("Idle", true);
+        }
 
+        if (Input.GetAxis("Horizontal") < - 0.1f)
+        {
+            Animator.SetBool("Running", true);
+            Animator.SetBool("Idle", false);
+            this.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            Animator.SetBool("Running", true);
+            Animator.SetBool("Idle", false);
+            this.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Animator.SetBool("Dead", true);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Animator.SetBool("Dead", false);
+        }
     }
 }
