fb377de0a96281cbb0cdf4210e6b38117e3352b6
sohan <sohan>
2016-05-18 17:29:47 -0400

    Lag Fix

--- Assets/Scripts/NetworkCharacter.cs
+++ Assets/Scripts/NetworkCharacter.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkCharacter : MonoBehaviour {
+public class NetworkCharacter : Photon.MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Vector3 realPosition = Vector3.zero;
+    public Vector3 positionAtLastPacket = Vector3.zero;
+    public double currentTime = 0.0;
+    public double currentPacketTime = 0.0;
+    public double lastPacketTime = 0.0;
+    public double timeToReachGoal = 0.0;
+
+    // Use this for initialization
+    void Start () {
+      //  realPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (photonView.isMine)
+        {
+            //do nothing
+
+        }
+        else
+        {
+            timeToReachGoal = currentPacketTime - lastPacketTime;
+            currentTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(positionAtLastPacket, realPosition, (float)(currentTime / timeToReachGoal));
+        }
 	}
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -18,14 +35,15 @@ public class NetworkCharacter : MonoBehaviour {
         if (stream.isWriting) // Our player. We need to send our actual position to the network.
         {
             stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
         }
         else
         {
             //this is someone else's player. We need to receive their position (as of a few milliseconds ago, and update our version of that player.
-
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
+            currentTime = 0.0;
+            positionAtLastPacket = transform.position;
+            realPosition = (Vector3)stream.ReceiveNext();
+            lastPacketTime = currentPacketTime;
+            currentPacketTime = info.timestamp;
 
         }
 
