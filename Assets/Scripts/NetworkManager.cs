f8e1a9eeac7393eca3139a9a163e31f3de925496
sohan <sohan>
2016-05-20 17:14:19 -0400

    Photon Max player limit.
    To Do: Create new room on joinroom fail

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
+    int currentRoom = 0;
+    string[] roomList;
 
     // Use this for initialization
     void Start()
@@ -30,23 +32,36 @@ public class NetworkManager : MonoBehaviour {
 
     void OnConnectedToMaster()
     {
+        if (PhotonNetwork.countOfPlayers == 4)
+        {
 
-        PhotonNetwork.JoinRandomRoom();
+        }
+        else
+            PhotonNetwork.JoinRandomRoom();
+            
         Debug.Log("OnConnectedToMaster");
     }
 
+    void OnJoiledLobby()
+    {
+        Debug.Log("room length : " + PhotonNetwork.GetRoomList().Length);
+    }
 
     void OnPhotonRandomJoinFailed()
     {
+        currentRoom++;
         Debug.Log("OnPhotonRandomJoinFailed");
-        PhotonNetwork.CreateRoom(null);
+        Debug.Log(PhotonNetwork.ServerAddress);
+        //PhotonNetwork.GetRoomList();
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 1}; //Set max players
+        PhotonNetwork.CreateRoom("Room" + currentRoom, roomOptions, TypedLobby.Default);
 
     }
+    
 
     void OnJoinedRoom()
     {
-        Debug.Log("OnJoinedRoom");
-
+        Debug.Log("OnJoinedRoom" + PhotonNetwork.room.name);
         SpawnMyPlayer();
     }
 
@@ -54,7 +69,6 @@ public class NetworkManager : MonoBehaviour {
     {
         GameObject MyPlayerGO = (GameObject)PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
         ((MonoBehaviour)MyPlayerGO.GetComponent("Controls")).enabled = true;
-
     }
 
 }
