7ca26dc6f2d696b5999ae6b5ee3b3e092df3d867
Eric Phangnanouvong <Viruth_e-phang@hotmail.com>
2016-06-03 16:19:56 -0400

    new sprites for animations

---
+++ Assets/Scripts/FlipSprite.cs
@@ -0,0 +1,23 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class FlipSprite : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
