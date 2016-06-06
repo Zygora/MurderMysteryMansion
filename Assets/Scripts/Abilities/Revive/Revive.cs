35661222cbcd231fe5a3d36dc4e81764f76a9cc7
Artem Nikolaev <oektann@gmail.com>
2016-06-06 15:23:29 -0400

    Abilities

---
+++ Assets/Scripts/Abilities/Revive/Revive.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Move to Controls script and disable player's movement while he's reviving
+public class Revive : MonoBehaviour
+{
+    bool nearDownedWimp;
+    public float reviveTime = 2.0f;
+    float reviveTimePassed;
+    GameObject downedWimp;
+
+    void Update()
+    {
+        if ((nearDownedWimp) && (Input.GetKey(KeyCode.R)))
+        {
+            reviveTimePassed += Time.deltaTime;
+            if (reviveTimePassed > reviveTime)
+            {
+                downedWimp.tag = "Wimp";
+                downedWimp.GetComponent<Animator>().enabled = true;
+                reviveTimePassed = 0;
+                nearDownedWimp = false;
+                print("revived");
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DownedWimp")
+        {
+            nearDownedWimp = true;
+            downedWimp = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DownedWimp")
+        {
+            nearDownedWimp = false;
+        }
+    }
+}
--- Assets/Scripts/Abilities/ThirdEye.cs
+++ Assets/Scripts/Abilities/ThirdEye.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThirdEye : MonoBehaviour {
+public class ThirdEye : MonoBehaviour
+{
     private float time = 0;
     private float cooldown = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-            if (Input.GetKeyDown(KeyCode.Space) && Time.time > time + cooldown)
-            {
-                Debug.Log("Third Eye Used");
-                time = Time.time;
-                cooldown = 5;
-            }
-	}
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > time + cooldown)
+        {
+            Debug.Log("Third Eye Used");
+            time = Time.time;
+            cooldown = 5;
+        }
+    }
 
 
 }
