ea3efa4d40adaeb484c1dc4ac9d722ec9f3e338e
sohan <sohan>
2016-06-20 19:08:22 -0400

    working on making murderer spawn static, randomly chooses player to be murderer

--- Assets/Scripts/RandomSpawnPlayers.cs
+++ Assets/Scripts/RandomSpawnPlayers.cs
@@ -7,30 +7,41 @@ public class RandomSpawnPlayers : MonoBehaviour {
     public bool player2Spawned = false;
     public bool player3Spawned = false;
     public bool player4Spawned = false;
+    public bool murdererSpawned = false;
     private GameObject player1Camera;
     private GameObject player2Camera;
     private GameObject player3Camera;
     private GameObject player4Camera;
     private int random;
+    private int t;
     private int randomMax;
     // Use this for initialization
     void Start () {
         RoomPositions = new List<Transform>();
+
         for (int x = 0; x < 25; x++)
         {
-            RoomPositions.Add(this.gameObject.transform.GetChild(x));
+            if (this.gameObject.transform.GetChild(x).name != "MurdererStart(Clone)")
+            {
+                RoomPositions.Add(this.gameObject.transform.GetChild(x));
+                //Debug.Log(x);
+            }
         }
-       randomMax = 25;
+
+        //Debug.Log(RoomPositions.Count+"yo");
+        randomMax = 24;
 
         player1Camera = GameObject.FindGameObjectWithTag("Player1Camera");
         player2Camera = GameObject.FindGameObjectWithTag("Player2Camera");
         player3Camera = GameObject.FindGameObjectWithTag("Player3Camera");
         player4Camera = GameObject.FindGameObjectWithTag("Player4Camera");
+        t = Random.Range(0, 4);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(player1Spawned == false) {
+
+        if (player1Spawned == false) {
             random = Random.Range(0, randomMax);
             Instantiate(Resources.Load("MainPlayer_1"), RoomPositions[random].position, Quaternion.identity);
             player1Camera.transform.position = new Vector3(RoomPositions[random].position.x, RoomPositions[random].position.y, -15);
@@ -69,5 +80,25 @@ public class RandomSpawnPlayers : MonoBehaviour {
             player4Spawned = true;
         }
 
+        if (t == 0 && murdererSpawned == false)
+        {
+            GameObject.FindGameObjectWithTag("Player1").GetComponent<MurdererScripts>().enabled = true;
+            murdererSpawned = true;
+        }
+        if (t == 1 && murdererSpawned == false)
+        {
+            GameObject.FindGameObjectWithTag("Player2").GetComponent<MurdererScripts>().enabled = true;
+            murdererSpawned = true;
+        }
+        if (t == 2 && murdererSpawned == false)
+        {
+            GameObject.FindGameObjectWithTag("Player3").GetComponent<MurdererScripts>().enabled = true;
+            murdererSpawned = true;
+        }
+        if (t == 3 && murdererSpawned == false)
+        {
+            GameObject.FindGameObjectWithTag("Player4").GetComponent<MurdererScripts>().enabled = true;
+            murdererSpawned = true;
+        }
     }
 }
