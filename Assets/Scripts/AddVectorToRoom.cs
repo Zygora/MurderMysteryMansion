6498305ae1eac85ac557a689ed7afe426c06c454
Artem Nikolaev <oektann@gmail.com>
2016-07-27 17:09:10 -0400

    testing vectors

---
+++ Assets/Scripts/AddVectorToRoom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddVectorToRoom : MonoBehaviour {
+    public Vector2 roomCoordinate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
