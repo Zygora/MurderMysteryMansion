b88e4122f897ecc09d83864f50f75c65a6cf7452
Artem Nikolaev <oektann@gmail.com>
2016-07-27 22:43:20 -0400

    third eye working. bugs to fix: too many arrows spawning, when left or right arrow spawns if you press the opposite driection another arrow spawns

--- Assets/Scripts/Abilities/ThirdEye.cs
+++ Assets/Scripts/Abilities/ThirdEye.cs
@@ -12,6 +12,10 @@ public class ThirdEye : MonoBehaviour
     public float thirdEyeCooldown = 1;
     private float timepassed;
     public Vector3 arrowSpawn;
+    private int leftArrow = 0;
+    private int rightArrow = 0;
+    private int upArrow = 0;
+    private int downArrow = 0;
 
     void Start() {
         arrow = Resources.Load("Arrow", typeof(GameObject)) as GameObject;
@@ -21,34 +25,28 @@ public class ThirdEye : MonoBehaviour
     {
         if (gameObject.tag == "Player1")
         {
-            if (currentRoomPos == Controls.currentPlayer2Pos || currentRoomPos == Controls.currentPlayer3Pos ||
-            currentRoomPos == Controls.currentPlayer4Pos)
-            {
-                CreateArrow();
-            }
-
             if (LeftRoomPos == Controls.currentPlayer2Pos || LeftRoomPos == Controls.currentPlayer3Pos ||
             LeftRoomPos == Controls.currentPlayer4Pos)
             {
-                CreateArrow();
+                CreateArrowLeft();
             }
 
             if (RightRoomPos == Controls.currentPlayer2Pos || RightRoomPos == Controls.currentPlayer3Pos ||
             RightRoomPos == Controls.currentPlayer4Pos)
             {
-                CreateArrow();
+                CreateArrowRight();
             }
 
             if (UpRoomPos == Controls.currentPlayer2Pos || UpRoomPos == Controls.currentPlayer3Pos ||
             UpRoomPos == Controls.currentPlayer4Pos)
             {
-                CreateArrow();
+                CreateArrowUp();
             }
 
             if ( BottomRoomPos == Controls.currentPlayer2Pos || BottomRoomPos == Controls.currentPlayer3Pos ||
             BottomRoomPos == Controls.currentPlayer4Pos)
             {
-                CreateArrow();
+                CreateArrowDown();
             }
         }
     }
@@ -63,13 +61,42 @@ public class ThirdEye : MonoBehaviour
         }
     }
 
-    void CreateArrow() {
-        arrowSpawn = new Vector3(this.transform.position.x, this.transform.position.y + 60, this.transform.position.z);
+    void CreateArrowLeft() {
         GameObject arrow1 = Instantiate(arrow);
         arrow1.transform.parent = this.transform;
-        arrow1.transform.position = arrowSpawn;
-        Vector3 dir1 = arrowSpawn - gameObject.transform.position;
-        float angle1 = Mathf.Atan2(dir1.y, dir1.x) * Mathf.Rad2Deg;
-        arrow1.transform.rotation = Quaternion.AngleAxis(angle1, Vector3.forward);
+        arrow1.transform.position = new Vector3(this.transform.position.x-20, this.transform.position.y + 60, this.transform.position.z);
+        arrow1.transform.eulerAngles = new Vector3(0, 0, 0);
+        Destroy(arrow1.gameObject, 1f);
+        leftArrow += 1;
+    }
+
+    void CreateArrowRight()
+    {
+        GameObject arrow1 = Instantiate(arrow);
+        arrow1.transform.parent = this.transform;
+        arrow1.transform.position = new Vector3(this.transform.position.x + 20, this.transform.position.y + 60, this.transform.position.z);
+        arrow1.transform.eulerAngles = new Vector3(0, 0, 180);
+        Destroy(arrow1.gameObject, 1f);
+        rightArrow += 1;
+    }
+
+    void CreateArrowDown()
+    {
+        GameObject arrow1 = Instantiate(arrow);
+        arrow1.transform.parent = this.transform;
+        arrow1.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 50, this.transform.position.z);
+        arrow1.transform.eulerAngles = new Vector3(0, 0, 90);
+        Destroy(arrow1.gameObject, 1f);
+        downArrow += 1;
+    }
+
+    void CreateArrowUp()
+    {
+        GameObject arrow1 = Instantiate(arrow);
+        arrow1.transform.parent = this.transform;
+        arrow1.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 80, this.transform.position.z);
+        arrow1.transform.eulerAngles = new Vector3(0, 0, 270);
+        Destroy(arrow1.gameObject, 1f);
+        upArrow += 1;
     }
 }
