b34322ac931dc79d7bbcc209e24d7a49f4bf6b47
Eric Phangnanouvong <viruth_e-phang@gmail.com>
2016-08-06 23:15:22 -0400

    New doppelganger fully functioning

--- Assets/Scripts/Abilities/Doppelganger/CloneScript.cs
+++ Assets/Scripts/Abilities/Doppelganger/CloneScript.cs
@@ -3,51 +3,45 @@ using System.Collections;
 
 public class CloneScript : MonoBehaviour
 {
-    public float timeBeforeDestroyed;   // Time after which this clone self-destructs
-    public float timePassed;            // Time since this clone was summoned
     public Vector2 direction;           // Direction in which this clone will be running to
     public float cloneSpeed;            // Speed of the clone
-    public Vector3 Destination;         // Point to which clone should travel after a player releases the ability button
-    bool destroy = false;
+    public Vector3 startPosition;
 
     void Start()
     {
-        timePassed = 0;
+     
     }
 
     void Update()
     {
-        if (gameObject.transform.position.x > Destination.x && direction == new Vector2(1, 0))
+        if (Doppelganger.cloneMoving==true)
         {
-            cloneSpeed = 0;
-            destroy = true;
+            gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Running", true);
+            gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Running", true);
+            gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Idle", false);
+            gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Idle", false);
+            transform.Translate(direction * Time.deltaTime * cloneSpeed);
+        }
+
+        if (Doppelganger.cloneMoving == false)
+        {
             // After clone reaches the destination change nimation to idle
             gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Running", false);
             gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Running", false);
             gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Idle", true);
             gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Idle", true);
-        }
-        if (gameObject.transform.position.x < Destination.x && direction == new Vector2(-1, 0))
-        {
-            cloneSpeed = 0;
-            destroy = true;
-            // After clone reaches the destination change nimation to idle
-            gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Running", false);
-            gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Running", false);
-            gameObject.GetComponent<Controls>().TorsoAnimator.SetBool("Idle", true);
-            gameObject.GetComponent<Controls>().LegsAnimator.SetBool("Idle", true);
+            Invoke("DestroyClone", 2f);
         }
 
-        if (destroy)
+        if (this.transform.position.x >= startPosition.x + 100 || this.transform.position.x <= startPosition.x - 100 ||
+            transform.position.x >= 570 || transform.position.x <= -570)
         {
-            timePassed += Time.deltaTime;
+            Doppelganger.cloneMoving = false;
         }
+    }
 
-        if (timePassed > timeBeforeDestroyed)
-        {
-            Destroy(gameObject);
-        }
-        // Move the clone in the direction
-        transform.Translate(direction * Time.deltaTime * cloneSpeed);
+    void DestroyClone() {
+        Destroy(this.gameObject);
+        Doppelganger.cloneAlive = false;
     }
 }
--- Assets/Scripts/Abilities/Doppelganger/Doppelganger.cs
+++ Assets/Scripts/Abilities/Doppelganger/Doppelganger.cs
@@ -4,20 +4,19 @@ using System.Collections;
 public class Doppelganger : MonoBehaviour
 {
     public int clonesAvailable = 2;      // The amount of clones available to player
-    public float cooldown = 30;           // Cooldown time player has to wait before he is able to summon another clone
-    public float arrowSpeed = 12;         // Speed with which arrow moves
-    public float timeBeforeDestroyed = 5;// Time after which a clone is destroyed after reaching the point
-    public GameObject arrow;         // Arrow object (Has to be in the scene and disabled)
-    Vector3 CloneDestination;        // Point to which clone should travel after a player releases the ability button
+    public float cooldown = 10;           // Cooldown time player has to wait before he is able to summon another clone
+    public float cloneSpeed = 20;
     Vector2 direction;               // Direction in which clone and arrow will be moving
     float timeSinceUsed;             // Time passed since the ability was last used
-    bool cloneUsed;                  // Flag that shows that the ability was used
-    bool down;                       // Flag that show that ability buttin was pressed
-    bool hold;                       // Flag that show that ability buttin was holded (We need them so player cant just hold button through cooldown and release it after and activate clone without arrow)
+    public bool cloneUsed = false;                  // Flag that shows that the ability was used
     private string ability;
+    public static bool cloneMoving = false;
+    public static bool cloneAlive = false;
+    private float cloneRefreshStart;
+    public float cloneRefreshTime = 15;
 
     void Start() {
-        arrow = GameObject.Find("DoppelgangerArrow");
+        
         //set input from input manager
         if (gameObject.tag == "Player1")
         {
@@ -41,17 +40,31 @@ public class Doppelganger : MonoBehaviour
     }
     void Update()
     {
-        // If clone used count time and enable to use the ability after cooldown time passed
-        if (cloneUsed)
+        if (Input.GetButtonDown(ability) && cloneMoving == true)
         {
-            timeSinceUsed += Time.deltaTime;
-            if (timeSinceUsed > cooldown)
+            cloneMoving = false;
+        }
+
+        if (clonesAvailable < 2) {
+            cloneRefreshStart += Time.deltaTime;
+            if (cloneRefreshStart > cloneRefreshTime)
             {
-                cloneUsed = false;
+                clonesAvailable++;
+                cloneRefreshStart = 0;
             }
         }
+
+        if (cloneUsed == true) {
+            timeSinceUsed += Time.deltaTime;
+            if(timeSinceUsed> cooldown)
+            {
+                cloneUsed = false;
+                timeSinceUsed = 0;
+            }
+        }
+
         // If ability can be used
-        if (!cloneUsed)
+        if (cloneAlive == false && cloneUsed == false)
         {
             // If there's another clone charge available
             if (clonesAvailable > 0)
@@ -59,61 +72,33 @@ public class Doppelganger : MonoBehaviour
                 // Player faces right
                 if (Input.GetButtonDown(ability) && (gameObject.GetComponent<Controls>().direction > 0))
                 {
-                    arrow.SetActive(true);
-                    direction = new Vector2(1, 0);
-                    arrow.transform.position = gameObject.transform.position;
-                    arrow.transform.position -= new Vector3(0, 1, 0);
-                    down = true;
+                    SetBoolsAndDirection();
+                    GameObject clone = Instantiate(Resources.Load("Clone"), this.transform.position, Quaternion.identity) as GameObject;
+                    clone.gameObject.GetComponent<CloneScript>().startPosition = this.transform.position;
+                    clone.gameObject.GetComponent<CloneScript>().direction = direction;
+                    clone.gameObject.GetComponent<CloneScript>().cloneSpeed = cloneSpeed;
+                    clone.transform.eulerAngles = new Vector3(0, 0, 0);
                 }
+
                 // Player faces left
-                if (Input.GetButtonDown(ability) && (gameObject.GetComponent<Controls>().direction < 0))
+                else if (Input.GetButtonDown(ability) && (gameObject.GetComponent<Controls>().direction < 0))
                 {
-                    arrow.SetActive(true);
-                    direction = new Vector2(-1, 0);
-                    arrow.transform.position = gameObject.transform.position;
-                    arrow.transform.position -= new Vector3(0, 1, 0);
-                    down = true;
-                }
-                // While player holds the ability button move the arrow and save the spot where it is
-                if (Input.GetButton(ability) && (down))
-                {
-                    arrow.transform.Translate(direction * Time.deltaTime * arrowSpeed);
-                    CloneDestination = arrow.transform.position;
-                    hold = true;
-                }
-                // After player releases the button
-                if (Input.GetButtonUp(ability) && (down) && (hold))
-                {
-                    // Disable the arrow
-                    arrow.SetActive(false);
-                    // Instantiate a clone
-                    GameObject clone = Instantiate(Resources.Load("Clone")) as GameObject;
-                    // If we need to flip the clone make it's speed negative so it will be moving in the right direction
-                    if (direction == new Vector2(-1, 0))
-                    {
-                        clone.transform.eulerAngles = new Vector3(0, 180, 0);
-                        clone.GetComponent<CloneScript>().cloneSpeed *= -1;
-                    }
-                    // Move clone to players posi tion
-                    clone.transform.position = gameObject.transform.position;
-                    // Pass the time after which it shoul be destroyed to the clone's script
-                    clone.GetComponent<CloneScript>().timeBeforeDestroyed = timeBeforeDestroyed;
-                    clone.GetComponent<CloneScript>().direction = direction;
-                    clone.GetComponent<CloneScript>().Destination = CloneDestination;
-                    // Activate running animations for the clone
-                    clone.GetComponent<Controls>().TorsoAnimator.SetBool("Running", true);
-                    clone.GetComponent<Controls>().LegsAnimator.SetBool("Running", true);
-                    clone.GetComponent<Controls>().TorsoAnimator.SetBool("Idle", false);
-                    clone.GetComponent<Controls>().LegsAnimator.SetBool("Idle", false);
-                    // Ignore collision with other players
-                    Physics2D.IgnoreCollision(clone.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
-                    clonesAvailable--;
-                    cloneUsed = true;
-                    timeSinceUsed = 0;
-                    down = false;
-                    hold = false;
+                    SetBoolsAndDirection();
+                    GameObject clone = Instantiate(Resources.Load("Clone"), this.transform.position, Quaternion.identity) as GameObject;
+                    clone.gameObject.GetComponent<CloneScript>().startPosition = this.transform.position;
+                    clone.gameObject.GetComponent<CloneScript>().direction = direction;
+                    clone.gameObject.GetComponent<CloneScript>().cloneSpeed = cloneSpeed;
+                    clone.transform.eulerAngles = new Vector3(0, 180, 0);
                 }
             }
         }
+    }
+
+    void SetBoolsAndDirection() {
+        direction = new Vector2(1, 0);
+        cloneMoving = true;
+        cloneAlive = true;
+        cloneUsed = true;
+        clonesAvailable--;
     }
 }
